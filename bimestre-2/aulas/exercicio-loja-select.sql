e4f7c48c166b1b0f9bfe4ea7389da24f5eddd52e
David Lima <david.lima@ifal.edu.br>
2024-08-08 08:59:22 -0300

    Update exercicio-loja-select.sql

--- bimestre-2/aulas/exercicio-loja-select.sql
+++ bimestre-2/aulas/exercicio-loja-select.sql
@@ -1,13 +1,14 @@
 # - Selecione todos os vendedores com comissão tipo B
-
+SELECT * FROM vendedor WHERE faixa_comissao = "B";
 # - Selecione todos os clientes de AL
-
+SELECT * FROM cliente WHERE estado = "AL";
 # - Selecione todos os pedidos que contenham o produto 4
-
+SELECT * FROM itempedido WHERE FK_cod_produto = 4;
 # - Selecione todos os pedidos com o prazo de
 # entrega maior que 12 dias
-
+SELECT * from pedido WHERE prazo_entrega > 12;
 # - Selecione todos os produtos do pedido 3
-
+SELECT * FROM itempedido WHERE FK_num_pedido = 3;
 # - Apresente os vendedores em ordem crescente
 # dos salarios
+SELECT * FROM vendedor ORDER BY salario_fixo;
