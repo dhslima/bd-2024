9b5d6d3ef4f00f99368914e1859fb00ccee5d0d0
David Lima <david.lima@ifal.edu.br>
2024-08-15 07:37:40 -0300

    Create exercicio-loja-select-3.sql

---
+++ bimestre-2/aulas/exercicio-loja-select-3.sql
@@ -0,0 +1,14 @@
+# GROUP BY
+# Quantos clientes de cada cidade a loja tem?
+
+# Quantos itens tem cada pedido?
+
+# Quantos pedidos cada vendedor fez?
+
+# Qual a soma de produtos e a média de produtos em cada pedido?
+
+# Qual o maior, menor e médio prazo de entrega de cada vendedor?
+
+# Quantos pedidos cada item está incluido?
+
+# Top 3 dos produtos mais vendidos
