edb8cd80ed1e8c67f5ea9806703df5b07896023d
David Lima <david.lima@ifal.edu.br>
2024-08-29 07:37:01 -0300

    Create loja-exercicio-multiselect.sql

---
+++ bimestre-2/aulas/loja-exercicio-multiselect.sql
@@ -0,0 +1,19 @@
+#Liste os nomes dos clientes que estão localizados na mesma cidade em que algum vendedor está baseado.
+
+#Liste os vendedores cujo salário fixo é maior que a média dos salários fixos de todos os vendedores.
+
+# Encontre os nomes dos clientes que já fizeram pelo menos um pedido.
+
+# Encontre os produtos que foram pedidos mais de 3 vezes.
+
+# Liste os vendedores que nunca realizaram um pedido.
+
+# Liste os pedidos cujo valor total é maior que o valor médio de todos os pedidos.
+
+# Encontre os clientes que não têm nenhum pedido associado.
+
+# Liste os nomes dos vendedores que têm uma faixa de comissão igual à de algum outro vendedor.
+
+# Liste os produtos cujo valor unitário é superior ao valor unitário médio de todos os produtos.
+
+# Encontre os pedidos que têm um prazo de entrega maior do que a média de prazo de entrega de todos os pedidos.
