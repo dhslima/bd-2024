e3d8494407548203381ca908bb9fd9874f85109b
David Lima <david.lima@ifal.edu.br>
2024-09-22 22:42:09 -0300

    Aula 21/09

---
+++ bimestre-3/aulas/inicio-join-exercicio.sql
@@ -0,0 +1,10 @@
+# Liste os nomes de todos os alunos e seus respectivos cursos
+
+# Liste os nomes de todos os professores e as disciplinas que eles ministram
+
+# Liste os nomes de todos os alunos e a nota mais alta que eles obtiveram em qualquer disciplina
+
+# Liste todas as disciplinas que têm alunos matriculados, mostrando o nome da disciplina e a quantidade de alunos matriculados em cada uma
+
+# DESAFIO!!
+# Encontra os alunos matriculados nas disciplinas ministradas por um professor cujo e-mail é "professor@exemplo.com"
