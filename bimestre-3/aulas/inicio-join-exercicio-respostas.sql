88a03e970eade6282e93d11bdb511b0e63524c1a
David Lima <david.lima@ifal.edu.br>
2024-09-26 09:22:58 -0300

    Update inicio-join-exercicio-respostas.sql

--- bimestre-3/aulas/inicio-join-exercicio-respostas.sql
+++ bimestre-3/aulas/inicio-join-exercicio-respostas.sql
@@ -18,14 +18,13 @@ INNER JOIN alunodisciplina ad ON d.id = ad.FK_disciplina_id
 INNER JOIN aluno a ON a.mat = ad.FK_aluno_mat
 WHERE a.nome = "João Silva";
 
-SELECT nome FROM disciplina WHERE id IN (1,3);
-SELECT FK_disciplina_id FROM alunodisciplina WHERE FK_aluno_mat = 1;
-SELECT mat FROM aluno WHERE nome = "João Silva";
+# Liste os nomes de todos os alunos e a nota mais alta que eles 
+# obtiveram em qualquer disciplina
 
 
-# Liste os nomes de todos os alunos e a nota mais alta que eles obtiveram em qualquer disciplina
-
-# Liste todas as disciplinas que têm alunos matriculados, mostrando o nome da disciplina e a quantidade de alunos matriculados em cada uma
+# Liste todas as disciplinas que têm alunos matriculados, mostrando o 
+# nome da disciplina e a quantidade de alunos matriculados em cada uma
 
 # DESAFIO!!
-# Encontra os alunos matriculados nas disciplinas ministradas por um professor cujo e-mail é "professor@exemplo.com"
+# Encontra os alunos matriculados nas disciplinas ministradas por um 
+# professor cujo e-mail é "professor.a@exemplo.com"
